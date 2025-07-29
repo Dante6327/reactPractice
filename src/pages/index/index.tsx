95ce6bf0e7ae8c18434fae1ee0658f9d73aef5fb
Dante6327 <jinpyeong93@gmail.com>
2025-07-30 01:04:00 +0900

    css 및 컴포넌트 일부 수정

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -6,9 +6,9 @@ const Index = () => {
       {/*공통 헤더 UI 부분*/}
       {/*공통 네비게이션 UI 부분*/}
       <div className={styles.page__contents}>
-        <div className={styles.page__contents_introBox}>
+        <div className={styles.page__contents__introBox}>
           <div className={styles.wrapper}>
-            <span className={styles.wrapper_title}>PhotoSplash</span>
+            <span className={styles.wrapper__title}>PhotoSplash</span>
             <span className={styles.wrapper__desc}>
               인터넷의 시각 자료 출처입니다. <br />
               모든 지역에 있는 크리에이터들의 지원을 받습니다.
